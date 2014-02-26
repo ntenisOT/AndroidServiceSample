2b2e1486edbdffbc7be7209395beb622547dd8ae
Georgios Larkou <ntenisOT@gmail.com>
2014-02-26 22:28:28 +0200

    Working Service

--- AndroidServiceSample/src/main/java/com/ntenisot/example/androidservicesample/MainActivity.java
+++ AndroidServiceSample/src/main/java/com/ntenisot/example/androidservicesample/MainActivity.java
@@ -1,27 +1,83 @@
 package com.ntenisot.example.androidservicesample;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.*;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView textStatus, textIntValue, textStrValue;
+    Button btnUpby1, btnUpby10;
+    Messenger mService = null;
+    boolean mIsBound;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PlaceholderFragment f = new PlaceholderFragment();
+
         if (savedInstanceState == null) {
+
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        textIntValue = (TextView) findViewById(R.id.textIntValue);
+        textStrValue = (TextView) findViewById(R.id.textStrValue);
+        btnUpby1 = (Button) findViewById(R.id.btnUpby1);
+        btnUpby10 = (Button) findViewById(R.id.btnUpby10);
+
+        btnUpby1.setOnClickListener(btnUpby1Listener);
+        btnUpby10.setOnClickListener(btnUpby10Listener);
+
+        restoreMe(savedInstanceState);
+
+        CheckIfServiceIsRunning();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("textStatus", textStatus.getText().toString());
+        outState.putString("textIntValue", textIntValue.getText().toString());
+        outState.putString("textStrValue", textStrValue.getText().toString());
+    }
+
+    private void restoreMe(Bundle state) {
+        if (state!=null) {
+            textStatus.setText(state.getString("textStatus"));
+            textIntValue.setText(state.getString("textIntValue"));
+            textStrValue.setText(state.getString("textStrValue"));
+        }
+    }
+
+    private void CheckIfServiceIsRunning() {
+        //If the service is running when the activity starts, we want to automatically bind to it.
+        if (MyService.isRunning()) {
+            doBindService();
         }
     }
 
@@ -40,10 +96,74 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_start_service) {
+            startService(new Intent(MainActivity.this, MyService.class));
+            return true;
+        }
+        else if (id == R.id.action_stop_service) {
+            doUnbindService();
+            stopService(new Intent(MainActivity.this, MyService.class));
+            return true;
+        }
+        else if(id == R.id.action_bind_service) {
+            doBindService();
+            return true;
+        }
+        else if (id == R.id.action_unbind_service) {
+            doUnbindService();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private View.OnClickListener btnUpby1Listener = new View.OnClickListener() {
+        public void onClick(View v){
+            sendMessageToService(1);
+        }
+    };
+
+    private View.OnClickListener btnUpby10Listener = new View.OnClickListener() {
+        public void onClick(View v){
+            sendMessageToService(10);
+        }
+    };
+
+    private void sendMessageToService(int intvaluetosend) {
+        if (mIsBound) {
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, MyService.MSG_SET_INT_VALUE, intvaluetosend, 0);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                }
+            }
+        }
+    }
+
+    private void doBindService() {
+        bindService(new Intent(this, MyService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        textStatus.setText("Binding.");
+    }
+
+    private void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, MyService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service has crashed.
+                }
+            }
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            textStatus.setText("Unbinding.");
+        }
     }
 
     /**
@@ -59,6 +179,53 @@ public class MainActivity extends ActionBarActivity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+            textStatus.setText("Attached.");
+            try {
+                Message msg = Message.obtain(null, MyService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            } catch (RemoteException e) {
+                // In this case the service has crashed before we could even do anything with it
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+            mService = null;
+            textStatus.setText("Disconnected.");
+        }
+    };
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MyService.MSG_SET_INT_VALUE:
+                    textIntValue.setText("Int Message: " + msg.arg1);
+                    break;
+                case MyService.MSG_SET_STRING_VALUE:
+                    String str1 = msg.getData().getString("str1");
+                    textStrValue.setText("Str Message: " + str1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            doUnbindService();
+        } catch (Throwable t) {
+            Log.e("MainActivity", "Failed to unbind from the service", t);
         }
     }
 
